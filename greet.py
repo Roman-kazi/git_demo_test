5776937b18494da1cea49549834def5338e22857
Roman-kazi <rpgk2016@gmail.com>
2020-11-25 13:12:10 +0530

    i don't want play function

--- greet.py
+++ greet.py
@@ -1,17 +1,6 @@
-import random
 
-def greet(name):
-    print("Hello,", name)
 
-greet(input("Enter name: "))
+def greet():
+    print("Hello xyz")
 
-def play():
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
-    print("What is ",a," + ",b)
-    ans = int(input())
-    if a+b == ans:
-        print("Nice")
-    else:
-        print("Go to elementry school")
-play()
+greet()
